412e6de571310923b676d3c2f668e676fb4cfb5b
Lucas Steinmann <lucas@steinm.net>
2018-03-25 01:27:07 +0000

    Added GET to API and make in runnable.

--- webserver/api.py
+++ webserver/api.py
@@ -5,12 +5,18 @@ import numpy as np
 
 app = Flask(__name__)
 
-if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000)
-
 @app.route("/image", methods=["POST"])
 def index():
 	image = request.files["image"]
 	nparr = np.fromstring(image.stream.read(), np.uint8)
 	i = cv2.imdecode(nparr, cv2.CV_LOAD_IMAGE_COLOR)
 	return "%d" % i.shape[0]
+
+@app.route("/image", methods=["GET"])
+def default():
+	return "Use POST plz"
+
+
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=9999)
